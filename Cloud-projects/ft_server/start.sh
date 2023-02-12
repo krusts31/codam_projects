fb8c3510d54ff6ab7f58dfeb5bd9cfc258b26bee
SEGFAULT <segfault@pop-os.localdomain>
2023-02-12 13:33:43 +0200

    works

--- Cloud-projects/ft_server/start.sh
+++ Cloud-projects/ft_server/start.sh
@@ -1,11 +1,5 @@
-openrc && \
-	echo "" > /run/openrc/softlevel && \
-	/etc/init.d/mariadb setup && \
-	rc-service mariadb start && \
-	wp core download --path=/wp && \
-	wp config create --path=/wp --dbname=wordpress --dbuser=root --dbpass= && \
-	wp db create --path=/wp && \
-	wp core install --path=/wp --url="/" --title="hello world" --admin_user="alkrusts" --admin_password="password" --admin_email="alkrusts@student.codam.nl" && \
-	wp  --path=/wp plugin install "WP Mail SMTP" && \
-	wp  --path=/wp plugin activate "wp-mail-smtp" && \
-	mariadb -e "USE wordpress;UPDATE wp_options SET option_value='https://localhost/wordpress' WHERE option_name='siteurl' OR option_name='home';"
+echo 'rc_provide="loopback net"' >> /etc/rc.conf
+rc-service mariadb restart
+rc-service php-fpm82 restart
+rc-service nginx restart
+/bin/sh
